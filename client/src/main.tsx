5db87c13626c3c79e05ab56d708a117441b09531
ishonchmarketxarid-xaridlar <ishonchmarketxarid@gmail.com>
2026-03-12 02:15:20 -0700

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,4 +2,12 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+let userId = localStorage.getItem("userId");
+
+if (!userId) {
+  userId = Math.floor(Math.random() * 100000000).toString();
+  localStorage.setItem("userId", userId);
+}
+
 createRoot(document.getElementById("root")!).render(<App />);
+
